8e5ff98baae503d3b38265ab18b693a45b603ba9
Moon-YJ <tunjeong@gmail.com>
2023-11-14 12:18:22 +0900

    시간생성자 new Date()

--- js/main.js
+++ js/main.js
@@ -1,55 +1,32 @@
-/*
-  - JS에서 객체를 생성하는 방법 두가지
-    1. 객체 리터럴식으로 생성
-    2. 생성자를 통한 인스턴스 생성
+const now = new Date(); // 현재 시간 인스턴스객체 반환
+const year = now.getFullYear(); // 시간 인스턴스에서 메서드로 연도 반환
+const month = now.getMonth(); // 월의 값이 아닌 순서값 반환
+const monthName = [
+	'1월',
+	'2월',
+	'3월',
+	'4월',
+	'5월',
+	'6월',
+	'7월',
+	'8월',
+	'9월',
+	'10월',
+	'11월',
+	'12월',
+];
+const date = now.getDate(); // 일 반환
+const day = now.getDay(); // 순서값 반환: 일=0, 월=1, 화=2
+const hour = now.getHours(); // 시간 반환
+const min = now.getMinutes(); // 분 반환
+const sec = now.getSeconds(); // 초 반환
+const time = now.getTime(); // 표준시간이 지정된 시점으로부터의 밀리세컨드값 반환
 
-  - ES6에서의 객체지향
-   : 동일한 형식의 객체를 대량으로 뽑아내야될때 객체생성을 자동화
-   : 생성자 - 객체를 생성해주는 함수 (붕어빵 틀)
-   : 인스턴스 - 생성자를 통해서 복사된 객체 (만들어진 붕어빵들)
-   : 프로토타입 - 생성자 안에 자동으로 추가되는 공통의 저장공간 (각 인스턴스에 활용될 함수 등록: 메서드)
-   : 같은 생성자를 통해서 복사된 인스턴스들은 프로토타입에 등록된 메서드를 공유
-   : 생성자 안쪽의 this - 해당 생성자를 통해서 앞으로 복사될 인스턴스 객체를 지칭
-*/
-
-// 생성자를 통한 인스턴스 생성
-class Student {
-	constructor(name, age) {
-		this.name = name;
-		this.age = age;
-		this.school = 'uni';
-	}
-	// 프로토타입에 각 인스턴스들이 공통으로 활용할 함수 등록(메서드)
-	plusAge() {
-		this.age = this.age + 1;
-	}
-	changeSchool(txt) {
-		this.school = txt;
-	}
-}
-
-// 생성자를 통해서 두개의 인스턴스 객체 복사
-const s1 = new Student('David', 20);
-const s2 = new Student('Amy', 23);
-//s1.plusAge();
-// 인스턴스가 생성된 이후 프로토타입의 메서드를 호출해서 일괄적으로 인스턴스의 구조를 변경 가능
-[s1, s2].forEach((el) => {
-	el.plusAge();
-	el.changeSchool('college');
-});
-console.log(s1);
-console.log(s2);
-
-// 리터럴식으로 객체 생성
-/*
-  const s1 = {
-    name: 'David',
-    age: 20,
-    school: 'uni'
-  }
-  const s2 = {
-    name: 'Amy',
-    age: 23,
-    school: 'uni'
-  }
-*/
+console.dir(now);
+console.log(year);
+console.log(monthName[month]);
+console.log(day, date);
+console.log(hour);
+console.log(min);
+console.log(sec);
+console.log(time);
